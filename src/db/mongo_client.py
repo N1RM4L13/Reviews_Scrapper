e3fcd6ad3610d1e3db9c80c48b93e4f5455d148e
Nirmal <n1rm4l13@gmail.com>
2024-05-09 17:33:01 +0530

    connected db

---
+++ src/db/mongo_client.py
@@ -0,0 +1,51 @@
+from bson import ObjectId
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class BaseModel:
+
+    def __init__(cls, client, database, collection):
+        cls.__client = client
+        cls.__collection = collection
+        cls.__db = database
+
+    @classmethod
+
+
+    @classmethod
+    def get_collection(cls):
+        # with open('../../../config.json') as handle:
+        #     mongo_conn = json.loads(handle.read())
+        # cls.__client = AsyncIOMotorClient(mongo_conn['conn_string'])
+        return cls.__client[cls._db][cls.__collection]
+
+    @classmethod
+    async def insert_one(cls, document):
+        result = await cls.get_collection().insert_one(document)
+        return result.inserted_id
+
+    @classmethods
+    async def find_one(cls, document):
+        result = await cls.get_collection().find_one(document)
+        return result
+
+    @classmethod
+    async def find_all(cls, document):
+        result = await cls.get_collection().find_all(document)
+        return result
+
+    @classmethod
+    async def update_one(cls, old_document, new_document):
+        result = await cls.get_collection().update_one(old_document, {'$set': new_document})
+        return result
+
+    @classmethod
+    async def search_by_id(cls, id: str):
+        result = await cls.get_collection().find_one({"_id": ObjectId(id)})
+        return result
+
+    @classmethod
+    async def find(cls, document):
+        cursor = cls.get_collection().find(document)
+        result = await cursor.to_list()
+        return result
